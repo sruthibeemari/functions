84155b4da3890369ec74545225e6c8e6089b900e
Your Name <you@example.com>
2024-10-05 23:40:34 +0530

    added all files

---
+++ tskscript.js
@@ -0,0 +1,34 @@
+//1st   adding two numbers by taking them as argument
+function sum(a,b) {
+    console.log(a+b);
+    
+    
+}
+sum(10,20)
+//2    sub
+function sub(a,b) {
+    console.log(b-a);
+    
+    
+}
+sub(5,20)
+//3rd
+function mul(a,b) {
+    console.log(a*b);
+    
+    
+}
+mul(6,6)
+//4th
+function div(a,b) {
+    if(b===0){
+        console.log("Can't divide by zero");
+        
+    }
+    else{
+        console.log(a/b);
+        
+    }
+    
+}
+div(9,0)
